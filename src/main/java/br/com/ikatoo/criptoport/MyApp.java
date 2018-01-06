3685ad713424fa76c4de0cdc657581e0f600230c
mckatoo <mckatoo@localhost>
2018-01-06 00:21:37 -0200

    ADD JERSEY - CRIAR CONTROLLERS - AULA 5

---
+++ src/main/java/br/com/ikatoo/criptoport/MyApp.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.ikatoo.criptoport;
+
+import javax.ws.rs.ApplicationPath;
+import org.glassfish.jersey.server.ResourceConfig;
+
+/**
+ *
+ * @author mckatoo
+ */
+
+@ApplicationPath("rest")
+public class MyApp extends ResourceConfig {
+    public MyApp(){
+        packages("br.com.ikatoo.criptoport.controllers");
+    }
+}
---
+++ src/main/java/br/com/ikatoo/criptoport/controllers/Usuarios.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.ikatoo.criptoport.controllers;
+
+/**
+ *
+ * @author mckatoo
+ */
+
+public class Usuarios {
+    
+}
